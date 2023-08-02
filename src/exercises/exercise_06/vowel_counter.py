3a332a2e6823c12a35ec9e6966a68b8ab0601e4b
Victor Cazotti <victor.cazotti@enviabybus.com.br>
2023-08-02 18:32:05 -0300

    [mdc-mid-test] Add Exercise 6 - Vowel Control

---
+++ src/exercises/exercise_06/vowel_counter.py
@@ -0,0 +1,25 @@
+'''
+Exercise 6: Vowel Counter
+
+Create a function that counts the number of vowels in a string. The program should ask the
+user for a sentence and display how many vowels it has.
+'''
+
+class VowelCounter:
+  def count_vowels(self, sentence):
+    vowels = "aeiouAEIOU"
+    vowel_count = 0
+    for char in sentence:
+        if char in vowels:
+            vowel_count += 1
+    return vowel_count
+
+  def count_and_display_vowels(self, sentence):
+    vowel_count = self.count_vowels(sentence)
+    return f"The sentence has {vowel_count} vowels."
+
+def main() :
+  vowel_counter = VowelCounter()
+  user_input = input("Enter a sentence: ")
+  result = vowel_counter.count_and_display_vowels(user_input)
+  print(result)
