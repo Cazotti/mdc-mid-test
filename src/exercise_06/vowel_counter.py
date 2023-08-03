a3583c5f0455e27be48ee8be639b00948b55a767
Victor Cazotti <victor.cazotti@enviabybus.com.br>
2023-08-03 10:20:01 -0300

    [mdc-mid-test] Optimization of exercise 06

--- src/exercise_06/vowel_counter.py
+++ src/exercise_06/vowel_counter.py
@@ -6,17 +6,17 @@ user for a sentence and display how many vowels it has.
 '''
 
 class VowelCounter:
+  def count_and_display_vowels(self, sentence):
+    vowel_count = self.count_vowels(sentence)
+    return f"The sentence has {vowel_count} vowel{'s' if vowel_count != 1 else ''}."
+
   def count_vowels(self, sentence):
-    vowels = 'aeiouAEIOU'
+    vowels = set("aeiouAEIOU")
     vowel_count = 0
     for char in sentence:
       if char in vowels:
         vowel_count += 1
     return vowel_count
-
-  def count_and_display_vowels(self, sentence):
-    vowel_count = self.count_vowels(sentence)
-    return f'The sentence has {vowel_count} vowels.'
 
 def main() :
   vowel_counter = VowelCounter()
--- src/exercise_06/vowel_counter.test.py
+++ src/exercise_06/vowel_counter.test.py
@@ -7,17 +7,17 @@ class TestVowelCounter(unittest.TestCase):
     self.vowel_counter = VowelCounter()
 
   def test_count_vowels(self):
-    self.assertEqual(self.vowel_counter.count_vowels('Hello'), 2)
-    self.assertEqual(self.vowel_counter.count_vowels('Python is awesome'), 6)
-    self.assertEqual(self.vowel_counter.count_vowels('AEIOUaeiou'), 10)
     self.assertEqual(self.vowel_counter.count_vowels(''), 0)
+    self.assertEqual(self.vowel_counter.count_vowels('AEIOUaeiou'), 10)
+    self.assertEqual(self.vowel_counter.count_vowels('Darkness'), 2)
+    self.assertEqual(self.vowel_counter.count_vowels('A nut for a jar of tuna'), 8)
 
   def test_count_and_display_vowels(self):
-    result = self.vowel_counter.count_and_display_vowels('Hello, how are you?')
-    self.assertEqual(result, 'The sentence has 7 vowels.')
+    result = self.vowel_counter.count_and_display_vowels('We are going on holiday tomorrow')
+    self.assertEqual(result, 'The sentence has 12 vowels.')
 
-    result = self.vowel_counter.count_and_display_vowels('Python is fun!')
-    self.assertEqual(result, 'The sentence has 3 vowels.')
+    result = self.vowel_counter.count_and_display_vowels('I phoned her but she was not there.')
+    self.assertEqual(result, 'The sentence has 10 vowels.')
 
   def test_count_and_display_vowels_empty(self):
     result = self.vowel_counter.count_and_display_vowels('')
