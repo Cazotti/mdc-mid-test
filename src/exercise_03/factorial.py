e0a32a6dabc38af69195de8cc52b999d6739bbd7
Victor Cazotti <victor.cazotti@enviabybus.com.br>
2023-08-03 10:03:03 -0300

    [mdc-mid-test] Optimization of exercise 03

--- src/exercise_03/factorial.py
+++ src/exercise_03/factorial.py
@@ -7,7 +7,10 @@ user to enter a number and displays the corresponding factorial.
 
 class Factorial :
   def calculate_factorial(self, n) :
-    return 1 if n == 0 or n == 1 else n * self.calculate_factorial(n - 1)
+    if n < 0: return None
+    factorial = 1
+    for i in range(1, n + 1): factorial *= i
+    return factorial
 
   def calculate_and_display(self, num) :
     try:
@@ -15,7 +18,10 @@ class Factorial :
       if num < 0:
         return 'Factorial is not defined for negative numbers.'
       result = self.calculate_factorial(num)
-      return f'The factorial of {num} is {result}'
+      if result is not None:
+        return f'The factorial of {num} is {result}'
+      else:
+        return f'Error: Unable to calculate factorial for negative number {num}.'
     except ValueError:
       return 'Error: Invalid input. Please enter a valid number.'
 
--- src/exercise_03/factorial.test.py
+++ src/exercise_03/factorial.test.py
@@ -9,19 +9,20 @@ class TestFactorialCalculator(unittest.TestCase) :
   def test_calculate_factorial(self) :
     self.assertEqual(self.factorial_calculator.calculate_factorial(0), 1)
     self.assertEqual(self.factorial_calculator.calculate_factorial(1), 1)
-    self.assertEqual(self.factorial_calculator.calculate_factorial(5), 120)
     self.assertEqual(self.factorial_calculator.calculate_factorial(10), 3628800)
+    self.assertEqual(self.factorial_calculator.calculate_factorial(5), 120)
+    self.assertEqual(self.factorial_calculator.calculate_factorial(7), 5040)
 
   def test_calculate_and_display_positive(self) :
-    result = self.factorial_calculator.calculate_and_display(5)
-    self.assertEqual(result, 'The factorial of 5 is 120')
+    result = self.factorial_calculator.calculate_and_display(8)
+    self.assertEqual(result, 'The factorial of 8 is 40320')
 
   def test_calculate_and_display_negative(self) :
-    result = self.factorial_calculator.calculate_and_display(-5)
+    result = self.factorial_calculator.calculate_and_display(-9)
     self.assertEqual(result, 'Factorial is not defined for negative numbers.')
 
   def test_calculate_and_display_invalid_input(self) :
-    result = self.factorial_calculator.calculate_and_display('abc')
+    result = self.factorial_calculator.calculate_and_display('python')
     self.assertEqual(result, 'Error: Invalid input. Please enter a valid number.')
 
 if __name__ == '__main__' :
