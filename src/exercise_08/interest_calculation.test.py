72de5f5722b3749ffc419ee5be4d27504d2adb75
Victor Cazotti <victor.cazotti@enviabybus.com.br>
2023-08-03 10:31:46 -0300

    [mdc-mid-test] Optimization of exercise 08

--- src/exercise_08/interest_calculation.test.py
+++ src/exercise_08/interest_calculation.test.py
@@ -10,19 +10,19 @@ class TestInterestCalculation(unittest.TestCase):
     self.interest_calculation = InterestCalculation()
 
   def test_calculate_final_value(self):
-    result = self.interest_calculation.calculate_final_value(1000, 5, 12)
-    self.assertEqual(result, 1795.8563260221301)
+    result = self.interest_calculation.calculate_final_value(1212, 6, 11)
+    self.assertEqual(result, 2300.737852702536)
 
-  @patch('builtins.input', side_effect=['1000', '5', '12'])
+  @patch('builtins.input', side_effect=['3432', '8', '12'])
   def test_calculate_and_display_final_value(self, mock_input):
     captured_output = StringIO()
     sys.stdout = captured_output
     self.interest_calculation.calculate_and_display_final_value()
     sys.stdout = sys.__stdout__
-    expected_output = 'The final value of the investment is: 1795.86\n'
+    expected_output = 'The final value of the investment is: 8642.36\n'
     self.assertEqual(captured_output.getvalue(), expected_output)
 
-  @patch('builtins.input', side_effect=['abc', '5', '12'])
+  @patch('builtins.input', side_effect=['python', '3', '12'])
   def test_calculate_and_display_final_value_invalid_input(self, mock_input):
     captured_output = StringIO()
     sys.stdout = captured_output
